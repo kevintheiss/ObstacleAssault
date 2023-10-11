05417fa52738e1ff9cabadcd560909a9bb98c1f7
GeekmasterK <ktheiss1990@gmail.com>
2023-10-11 11:09:50 -0700

    Added code to the MovingPlatform class to calculate the distance moved.

--- ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
@@ -25,4 +25,9 @@ public:
 
 	UPROPERTY(EditAnywhere, Category="Moving Platform")
 	FVector PlatformVelocity = FVector(100, 0, 0);
+
+	UPROPERTY(VisibleAnywhere)
+	float DistanceMoved = -1;
+
+	FVector StartLocation;
 };
