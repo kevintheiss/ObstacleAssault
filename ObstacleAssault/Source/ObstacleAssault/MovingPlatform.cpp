38f7a8159e906eb1a427458a0d794d3d42303127
GeekmasterK <ktheiss1990@gmail.com>
2023-10-11 11:18:21 -0700

    Added code to the MovingPlatform class to move it back and forth between two locations.

--- ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -27,13 +27,18 @@ void AMovingPlatform::Tick(float DeltaTime)
 	// Move platform forwards
 		// Get current location
 	FVector CurrentLocation = GetActorLocation();
-		// Add vector to that location
+	// Add vector to that location
 	CurrentLocation += (PlatformVelocity * DeltaTime);
-		// Set the location
+	// Set the location
 	SetActorLocation(CurrentLocation);
 	// Send platform back if gone too far
 		// Check how far we've moved
-	DistanceMoved = FVector::Dist(StartLocation, CurrentLocation);
-		// Reverse direction of motion if gone too far
+	float DistanceMoved = FVector::Dist(StartLocation, CurrentLocation);
+	// Reverse direction of motion if gone too far
+	if (DistanceMoved > MoveDistance)
+	{
+		PlatformVelocity = -PlatformVelocity;
+		StartLocation = CurrentLocation;
+	}
 }
 
