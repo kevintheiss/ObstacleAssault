9a78a159f294766d08a30d6ba1ae3b3ea5cb5097
GeekmasterK <ktheiss1990@gmail.com>
2023-10-12 08:56:55 -0700

    Created a function to store the platform movement code. Created a platform rotation function that currently prints the name of the platform to the console.

--- ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -27,17 +27,20 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// Move platform forwards
-		// Get current location
+	MovePlatform(DeltaTime);
+	RotatePlatform(DeltaTime);
+}
+
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{
 	FVector CurrentLocation = GetActorLocation();
-	// Add vector to that location
+
 	CurrentLocation += (PlatformVelocity * DeltaTime);
-	// Set the location
+
 	SetActorLocation(CurrentLocation);
-	// Send platform back if gone too far
-		// Check how far we've moved
+
 	float DistanceMoved = FVector::Dist(StartLocation, CurrentLocation);
-	// Reverse direction of motion if gone too far
+
 	if (DistanceMoved > MoveDistance)
 	{
 		float OverShoot = (DistanceMoved - MoveDistance);
@@ -50,3 +53,7 @@ void AMovingPlatform::Tick(float DeltaTime)
 	}
 }
 
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
+	UE_LOG(LogTemp, Display, TEXT("%s rotating"), *GetName());
+}
--- ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
@@ -23,6 +23,8 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+
 	UPROPERTY(EditAnywhere, Category="Moving Platform")
 	FVector PlatformVelocity = FVector(100, 0, 0);
 
@@ -30,4 +32,7 @@ public:
 	float MoveDistance = 100;
 
 	FVector StartLocation;
+
+	void MovePlatform(float DeltaTime);
+	void RotatePlatform(float DeltaTime);
 };
